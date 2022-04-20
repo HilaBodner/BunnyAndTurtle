da3c4aab419a01f477b8557f02e3fc1f2bf33c7d
HilaBodner <hilabodner@gmail.com>
2022-04-20 23:20:14 +0300

    Class and Main implementation

--- src/__init__.py
+++ src/__init__.py
@@ -7,24 +7,34 @@ from Road import Road
 
 havi = CompetingAnimal("Havi the bunny", 0, 3, 'B')
 tirza = CompetingAnimal("Tirza the turtle", 0, 1, 'T')
-
 all_animals = [havi, tirza]
-
 race_road = Road(20)
+race_length = race_road.length_in_steps
 
 
-async def compete(all_animals):
-    while not havi.has_won(race_road.length_in_steps) and not tirza.has_won(race_road.length_in_steps):
+def race_is_over(animals: list):
+    for animal in all_animals:
+        if animal.has_won(race_length):
+            return True
+    return False
+
+
+def print_result(animals: list):
+    for animal in animals:
+        if animal.has_won(race_length):
+            print(animal.name + ' won!')
+
+
+async def compete(animals: list):
+    while not race_is_over(animals):
         await asyncio.sleep(1)
         print("RACE STATUS")
         print('----------------------------------------------------')
-        for animal in all_animals:
+        for animal in animals:
             print(" ".join(animal.run()))
         print('----------------------------------------------------')
 
-    for animal in all_animals:
-        if animal.has_won(race_road.length_in_steps):
-            print(animal.name + ' won!')
+    print_result(animals)
 
 
 asyncio.run(compete(all_animals))
